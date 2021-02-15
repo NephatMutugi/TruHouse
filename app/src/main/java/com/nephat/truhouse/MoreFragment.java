851c0081e2b22d202bbd1d7e0e135ac34363f410
Nephat Mutugi <muchirinephat5@gmail.com>
2021-02-15 19:06:46 +0300

    Sign In

--- app/src/main/java/com/nephat/truhouse/MoreFragment.java
+++ app/src/main/java/com/nephat/truhouse/MoreFragment.java
@@ -19,6 +19,7 @@ public class MoreFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_more, container, false);
+        View view = inflater.inflate(R.layout.fragment_more, container, false);
+        return view;
     }
 }
