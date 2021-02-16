6321afce826ce4cda7d46aa1277e62a8decfc6bf
Nephat Mutugi <muchirinephat5@gmail.com>
2021-02-16 09:25:48 +0300

    Sign In

--- app/src/main/java/com/nephat/truhouse/MoreFragment.java
+++ app/src/main/java/com/nephat/truhouse/MoreFragment.java
@@ -1,5 +1,6 @@
 package com.nephat.truhouse;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,9 +8,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class MoreFragment extends Fragment {
+
+    private static final String TAG = "MoreFragment";
+
+    //Widgets
+    private Button btnSignIn;
 
     public MoreFragment() {
         // Required empty public constructor
@@ -20,6 +27,14 @@ public class MoreFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_more, container, false);
+        btnSignIn = view.findViewById(R.id.btn_sign_in);
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), SignInActivity.class);
+                startActivity(intent);
+            }
+        });
         return view;
     }
 }
--- app/src/main/java/com/nephat/truhouse/SignInActivity.java
+++ app/src/main/java/com/nephat/truhouse/SignInActivity.java
@@ -2,13 +2,28 @@ package com.nephat.truhouse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private Button mSignInBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        mSignInBtn = findViewById(R.id.btn_sign_in);
+        mSignInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
