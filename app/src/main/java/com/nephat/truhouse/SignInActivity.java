3264c605793c26776d2ef03bbc5891f36931227a
Nephat Mutugi <muchirinephat5@gmail.com>
2021-04-07 11:14:52 +0300

    Login and Register

--- app/src/main/java/com/nephat/truhouse/SignInActivity.java
+++ app/src/main/java/com/nephat/truhouse/SignInActivity.java
@@ -6,26 +6,40 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class SignInActivity extends AppCompatActivity {
 
     private Button mSignInBtn;
+    private TextInputEditText mSignInEmail, mSignInPassword;
+    private TextView mForgotPass, mLinkRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-        mSignInBtn = findViewById(R.id.btn_sign_in);
-       /* mSignInBtn.setOnClickListener(new View.OnClickListener() {
+        mSignInBtn = findViewById(R.id.sign_in_btn);
+        mLinkRegister = findViewById(R.id.link_reg_text);
+
+        mSignInBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(SignInActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
-        }); */
+        });
 
+        mLinkRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignInActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
