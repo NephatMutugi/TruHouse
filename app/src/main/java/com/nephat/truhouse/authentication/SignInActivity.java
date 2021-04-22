ce16ed4c488d5b25da9e21038b7db1fd374f851c
Nephat Mutugi <muchirinephat5@gmail.com>
2021-04-22 12:29:10 +0300

    Remember User login

--- app/src/main/java/com/nephat/truhouse/authentication/SignInActivity.java
+++ app/src/main/java/com/nephat/truhouse/authentication/SignInActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.textfield.TextInputEditText;
 import com.nephat.truhouse.MainActivity;
 import com.nephat.truhouse.R;
+import com.nephat.truhouse.apputil.AppConfig;
 import com.nephat.truhouse.models.ApiResponse;
 import com.nephat.truhouse.retrofitUtil.ApiClient;
 import com.nephat.truhouse.retrofitUtil.ApiInterface;
@@ -30,6 +32,9 @@ public class SignInActivity extends AppCompatActivity {
     private TextInputEditText mSignInEmail, mSignInPassword;
     private TextView mForgotPass, mLinkRegister;
 
+    private boolean isRememberUserLogin = false;
+    private AppConfig appConfig;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +45,11 @@ public class SignInActivity extends AppCompatActivity {
         mSignInPassword = findViewById(R.id.sign_in_pass);
         mSignInBtn = findViewById(R.id.sign_in_btn);
         mLinkRegister = findViewById(R.id.link_reg_text);
+
+        appConfig = new AppConfig(this);
+        if (appConfig.isUserLogin()){
+            String name = appConfig.getNameOfUser();
+        }
 
 
         mSignInBtn.setOnClickListener(new View.OnClickListener() {
@@ -81,6 +91,12 @@ public class SignInActivity extends AppCompatActivity {
                             if (response.body().getResultCode() == 1){
 
                                 String name = response.body().getName();
+
+                                if (isRememberUserLogin){
+                                    appConfig.updateUserLoginStatus(true);
+                                    appConfig.saveNameOfUser(name);
+                                }
+
                                 Intent intent = new Intent(SignInActivity.this, MainActivity.class);
                                 startActivity(intent);
                                 finish();
@@ -117,6 +133,10 @@ public class SignInActivity extends AppCompatActivity {
     }
 
 
+    public void checkBoxClicked(View view){
+
+        isRememberUserLogin = ((CheckBox) view).isChecked();
+    }
 
     private void toastMessage(String message) {
         Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/nephat/truhouse/fragments/MoreFragment.java
+++ app/src/main/java/com/nephat/truhouse/fragments/MoreFragment.java
@@ -2,15 +2,16 @@ package com.nephat.truhouse.fragments;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
 
 import com.nephat.truhouse.R;
+import com.nephat.truhouse.apputil.AppConfig;
 import com.nephat.truhouse.authentication.SignInActivity;
 
 
@@ -20,6 +21,9 @@ public class MoreFragment extends Fragment {
 
     //Widgets
     private Button btnSignIn;
+    private TextView mLogout;
+
+    private AppConfig appConfig;
 
     public MoreFragment() {
         // Required empty public constructor
@@ -31,6 +35,19 @@ public class MoreFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_more, container, false);
         btnSignIn = view.findViewById(R.id.btn_sign_in);
+        mLogout = view.findViewById(R.id.logout);
+        appConfig = new AppConfig(getActivity().getApplicationContext());
+
+        mLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                appConfig.updateUserLoginStatus(false);
+                Intent intent = new Intent(getActivity(), SignInActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
