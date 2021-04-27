d85f542372c6ef88fca6136aa8a3f9bfbf302910
Nephat Mutugi <muchirinephat5@gmail.com>
2021-04-27 17:28:17 +0300

    Upload Images to database

--- app/src/main/java/com/nephat/truhouse/UploadHousesActivity.java
+++ app/src/main/java/com/nephat/truhouse/UploadHousesActivity.java
@@ -3,9 +3,14 @@ package com.nephat.truhouse;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -19,36 +24,36 @@ import androidx.core.content.ContextCompat;
 import com.nephat.truhouse.retrofitUtil.ApiClient;
 import com.nephat.truhouse.retrofitUtil.ApiInterface;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 
-import okhttp3.MediaType;
-import okhttp3.MultipartBody;
-import okhttp3.RequestBody;
-import okhttp3.ResponseBody;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class UploadHousesActivity extends AppCompatActivity {
 
     private static final String TAG = "UploadHousesActivity";
-
-    private static final String BASE_URL = "http://192.168.100.2/realEstate/";
-
+    private static final String BASE_URL = "http://192.168.100.2/realEstate/upload.php";
     private static final int MY_PERMISSIONS_REQUEST = 100;
     private int PICK_IMAGE_FROM_GALLERY_REQUEST = 1;
+    private String houseType, housePrice, houseLocation, houseContact, houseDescription;
 
-    String filepath = "";
-    String filepath1 = "";
-    String filepath2 = "";
-    String filepath3 = "";
+    Bitmap bitmap;
+    boolean check = true;
 
     private EditText mType, mPrice, mLocation, mContact, mDescription;
     private ImageView mHouseImage;
-    private Button mSaveBtn;
-
+    private Button mSaveBtn, mSelectImageBtn;
 
     private Retrofit retrofit = ApiClient.getApiClient();
     private ApiInterface apiInterface = retrofit.create(ApiInterface.class);
@@ -65,6 +70,9 @@ public class UploadHousesActivity extends AppCompatActivity {
         mContact = findViewById(R.id.editContact);
         mDescription = findViewById(R.id.editDescription);
         mSaveBtn = findViewById(R.id.btnSubmit);
+        mSelectImageBtn = findViewById(R.id.btnSelectImage);
+        hideSoftKeyboard();
+
 
 
         if (ContextCompat.checkSelfPermission(UploadHousesActivity.this,
@@ -76,21 +84,172 @@ public class UploadHousesActivity extends AppCompatActivity {
 
         }
 
-        mHouseImage.setOnClickListener(new View.OnClickListener() {
+        mSelectImageBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
                 intent.setType("image/*");
                 intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(
-                        Intent.createChooser(intent, "Select Picture"),
-                        PICK_IMAGE_FROM_GALLERY_REQUEST
-                );
+                startActivityForResult(Intent.createChooser(
+                        intent, "Select Image From Gallery"),
+                        PICK_IMAGE_FROM_GALLERY_REQUEST);
             }
         });
 
+        mSaveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                houseType = String.valueOf(mType.getText());
+                housePrice = String.valueOf(mPrice.getText());
+                houseLocation = String.valueOf(mLocation.getText());
+                houseContact = String.valueOf(mContact.getText());
+                houseDescription = String.valueOf(mDescription.getText());
+
+                uploadImage();
+            }
+        });
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null && data.getData() != null){
+            Uri uri = data.getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private void uploadImage() {
+        ByteArrayOutputStream byteArrayOutputStream;
+        byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+        byte[] byteArray = byteArrayOutputStream.toByteArray();
+        final String convertImage = Base64.encodeToString(byteArray, Base64.DEFAULT);
+        class AsyncTaskUploadClass extends AsyncTask<Void, Void, String>{
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+
+                toastMessage("Image is uploading");
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                toastMessage("Image has been uploaded successfully");
+                mHouseImage.setImageResource(android.R.color.transparent);
+            }
+
+            @Override
+            protected String doInBackground(Void... voids) {
+                ImageProcessClass imageProcessClass = new ImageProcessClass();
+                HashMap<String, String> hashMap = new HashMap<>();
+                hashMap.put("image_path", convertImage);
+                hashMap.put("location", houseLocation);
+                hashMap.put("price", housePrice);
+                hashMap.put("house_type", houseType);
+                hashMap.put("contact", houseContact);
+                hashMap.put("description", houseDescription);
+
+                String finalData = imageProcessClass.ImageHttpRequest(BASE_URL, hashMap);
+
+                return finalData;
+            }
+        }
+        AsyncTaskUploadClass asyncTaskUploadClass = new AsyncTaskUploadClass();
+        asyncTaskUploadClass.execute();
+    }
+
+
+    public class ImageProcessClass{
+        public String ImageHttpRequest(String requestURL, HashMap<String, String> PData){
+            StringBuilder stringBuilder = new StringBuilder();
+
+            try {
+                URL url;
+                HttpURLConnection httpURLConnection;
+                OutputStream outputStream;
+                BufferedWriter bufferedWriter;
+                BufferedReader bufferedReader;
+                int responseCode;
+                url = new URL(requestURL);
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setReadTimeout(19000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.setDoOutput(true);
+                outputStream = httpURLConnection.getOutputStream();
+
+                bufferedWriter = new BufferedWriter(
+                        new OutputStreamWriter(outputStream, "UTF-8")
+                );
+
+                bufferedWriter.write(bufferedWriterDataFN(PData));
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                responseCode = httpURLConnection.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK){
+                    bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+                    stringBuilder = new StringBuilder();
+                    String responseCode2;
+                    while ((responseCode2 = bufferedReader.readLine()) != null){
+                        stringBuilder.append(responseCode2);
+                    }
+
+                }
+
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+            return stringBuilder.toString();
+        }
+
+        private String bufferedWriterDataFN(HashMap<String, String> hashMap) throws UnsupportedEncodingException{
+            StringBuilder stringBuilder;
+            stringBuilder = new StringBuilder();
+            for (Map.Entry<String, String> KEY :hashMap.entrySet()){
+                if (check){
+                    check = false;
+                } else {
+                    stringBuilder.append("&");
+                }
+                stringBuilder.append(URLEncoder.encode(KEY.getKey(), "UTF-8"));
+                stringBuilder.append("=");
+                stringBuilder.append(URLEncoder.encode(KEY.getValue(), "UTF-8"));
+            }
+
+            return stringBuilder.toString();
+        }
+    }
+
+    private void toastMessage(String message) {
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
+    }
+
+
+    private void hideSoftKeyboard()
+    {
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+    }
+
+}
+
+
+
+
+
+
+
+
+    /*
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -111,7 +270,6 @@ public class UploadHousesActivity extends AppCompatActivity {
         RequestBody houseLocation = RequestBody.create(MultipartBody.FORM, mLocation.getText().toString());
         RequestBody houseContact = RequestBody.create(MultipartBody.FORM, mContact.getText().toString());
         RequestBody houseDescription = RequestBody.create(MultipartBody.FORM, mDescription.getText().toString());
-
 
 
         //Create Retrofit Instance
@@ -147,11 +305,21 @@ public class UploadHousesActivity extends AppCompatActivity {
 
     }
 
-    private void toastMessage(String message) {
-        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
-    }
+    */
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
